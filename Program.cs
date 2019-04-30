fb4f12a690509d652b75a0addf25a2b89cfa19a0
ajugalushkin <50012581+ajugalushkin@users.noreply.github.com>
2019-04-30 14:55:59 +0300

    Debugged the program, added event handling.

--- Program.cs
+++ Program.cs
@@ -11,11 +11,24 @@ namespace СaesarСipherApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Type <text> <key> <operation (Encode or Decode)>separated by a space:");
-            string[] iv_concole_str = Console.ReadLine().Split();
-            CaesarCipher encodetext = new CaesarCipher(iv_concole_str[0], Convert.ToInt32(iv_concole_str[1]), iv_concole_str[2]);
-            Console.WriteLine(encodetext.ev_text);
-            Console.ReadKey();
+            for (; ; )
+            {
+                Console.WriteLine("Type <text> <key> <operation (Encode or Decode)>separated by a space:");
+                string[] iv_concole_str = Console.ReadLine().Split();
+                try
+                {
+                    CaesarCipher encodetext = new CaesarCipher(iv_concole_str[0], Convert.ToInt32(iv_concole_str[1]), iv_concole_str[2]);
+                    Console.WriteLine(encodetext.ev_text);
+                    Console.WriteLine("Continue?(y/n)");
+                    if(Console.ReadLine().ToUpper() != "Y") { break; }
+                }
+                catch
+                {
+                    Console.WriteLine("Wrong parametrs!!!");
+                    Console.ReadKey();
+                    break;
+                }
+            }
         }
     }
 }
